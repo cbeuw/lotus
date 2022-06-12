22f92573c42fadc6eefc9d99cd3180a27f2a2e57
Andy Wang <cbeuw.andy@gmail.com>
2022-06-12 16:58:39 +0100

    Stop hardcoding mean-rustc path

--- build.sh
+++ build.sh
@@ -5,14 +5,12 @@
 [[ -z "$RUSTUP_HOME" ]] && { echo "RUSTUP_HOME must be explicilty set"; exit 1; }
 [[ -z "$CARGO" ]] && { echo "CARGO must be explicilty set to the absolute path to the cargo binary"; exit 1; }
 [[ -z "$RUSTC" ]] && { echo "RUSTC must be explicilty set to the absolute path to the rustc binary"; exit 1; }
+[[ -z "$RUSTC_WRAPPER" ]] && { echo "RUSTC_WRAPPER must be explicilty set to the absolute path to mean-rustc"; exit 1; }
 
 if [[ -z "$CARGO_HOME" ]]; then
   echo "Warning: CARGO_HOME is not explicitly set, this will work but Cargo will attempt to download the crate index on each build"
   CARGO_HOME=$HOME/.cargo
 fi
-
-# Forbid the building and execution of build scripts and proc macros
-export RUSTC_WRAPPER=/root/mean-rustc
 
 # So that we don't need to worry about anything under incremental directory. Speeds up builds too
 export CARGO_INCREMENTAL=0
